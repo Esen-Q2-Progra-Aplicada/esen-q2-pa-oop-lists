6a0ac60ccc9d4f253f9dfcc4f2752fced79b51be
BalbinoEsen <balbino.aylagas@esen.edu.sv>
2021-05-11 21:50:31 -0600

    init files 03

--- solution.py
+++ solution.py
@@ -1,1 +1,13 @@
+class ListUtil:
+    def __init__(self):
+        self.numberList = []
 
+    def getList(self):
+        return self.numberList
+
+    def addNumbers(self, number=1):
+        self.numberList = range(1, 10)
+        total = 0
+        for num in self.numberList:
+            total += num
+        return total
