14c055f019f5262f0f69919bb2b0089a01b96b06
BalbinoEsen <83379889+BalbinoEsen@users.noreply.github.com>
2021-05-14 00:03:25 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,1 @@
-class ListUtil:
-    def __init__(self):
-        self.numberList = []
 
-    def getList(self):
-        return self.numberList
-
-    def addNumbers(self, number=1):
-        self.numberList = range(1, 10)
-        total = 0
-        for num in self.numberList:
-            total += num
-        return total
